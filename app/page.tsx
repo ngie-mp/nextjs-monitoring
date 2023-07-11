26f79913e9d7030d6d0237fa3b6fedc82e2107a3
Angie <angeles.morap@gmail.com>
2023-07-11 11:27:20 +0200

    [feat] use link router

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello world</h1>
+      <Link href='/about'>about page</Link>
     </main>
   )
 }
